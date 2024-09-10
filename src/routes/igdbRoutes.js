b29dfc59ffa1e83bfcedae40bfd5fc9b5b791522
JackHeinrich <JackHeinrich2005@gmail.com>
2024-09-10 16:35:25 -0400

    added get_games_by_search route

--- src/routes/igdbRoutes.js
+++ src/routes/igdbRoutes.js
@@ -19,14 +19,7 @@ router.get("/frontpage_games", async (req, res) => {
 
     // Make a request to the IGDB API with specified fields
     const response = await client
-      .fields([
-        "name",
-        "cover.url",
-        "first_release_date",
-        "genres.name",
-        "total_rating",
-        "summary",
-      ]) // Specify fields to fetch
+      .fields(["name", "cover.url"]) // Specify fields to fetch
       .limit(limit) // Set the number of results to fetch
       .offset(offset) // Set the offset for pagination
       .where("status = 0")
@@ -92,15 +85,14 @@ router.get("/game_by_id", async (req, res) => {
   }
 });
 
-// Define the route to fetch all games from IGDB
-router.get("/get_games_by_query", async (req, res) => {
+// Define the GET route to fetch game by name and genre using query parameters
+router.get("/get_games_by_search", async (req, res) => {
   try {
-    // Initialize the IGDB client with the Client ID and Access Token
     const client = igdb(CLIENT_ID, IGDB_ACCESS_TOKEN);
 
-    const queryParams = req.params;
+    let { name, genre } = req.query;
 
-    // Make a request to the IGDB API with specified fields
+    // Create a query to get all games with specified name or genre
     const response = await client
       .fields([
         "name",
@@ -109,26 +101,22 @@ router.get("/get_games_by_query", async (req, res) => {
         "genres.name",
         "total_rating",
         "summary",
-      ]) // Specify fields to fetch
-      .limit(limit) // Set the number of results to fetch
-      .offset(offset) // Set the offset for pagination
-      .where("status = 0")
+      ])
+      .limit(10)
+      .offset(0)
       .where("category = 0")
-      .where("total_rating_count > 5")
-      .sort("total_rating_count", "desc")
-      .request("/games"); // Endpoint to request
+      .where(genre ? `genres = (${genre})` : "")
+      .search(`${name ? name : ""}`)
+      .request("/games");
 
-    // Send the data back to the client
     res.json(response.data);
   } catch (error) {
-    // Handle errors, log only the error message and status
     console.error("Error response:", {
       message: error.message,
       status: error.response?.status,
       data: error.response?.data,
     });
 
-    // Send an error message to the client
     res.status(500).json({
       message: `Failed to fetch data from IGDB API: ${error.message}`,
     });
